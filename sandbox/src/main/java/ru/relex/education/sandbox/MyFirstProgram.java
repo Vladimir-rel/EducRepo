3f8f15b6478826c9e5ef5c14cdb2a81266efd84e
Kodenko <vladimir_kodenko@relex.ru>
2020-08-26 16:16:38 +0300

    Add some metods.

--- sandbox/src/main/java/ru/relex/education/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/relex/education/sandbox/MyFirstProgram.java
@@ -3,7 +3,25 @@ package ru.relex.education.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args){
+		hello("world");
+		hello("user");
+		hello("Vladimir");
 
-		System.out.println("Hello, word!");
+		double l = 5;
+		System.out.println("Площадь квдрпата со стороной " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + "  и " + b + " = " + area(a,b));
+
+	}
+	public static void hello(String some){
+		System.out.println("Hello, " + some + "!");
+	}
+	public static double area(double len){
+		return len * len;
+	}
+	public static double area(double a, double b){
+		return a * b;
 	}
 }
