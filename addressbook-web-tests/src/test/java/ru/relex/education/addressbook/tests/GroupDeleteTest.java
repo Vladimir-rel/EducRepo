7082098e46b23dde4d7be12fb0eb7e5c0d2d8c5f
Kodenko <vladimir_kodenko@relex.ru>
2020-09-08 17:46:48 +0300

    Add compare list elements.

--- addressbook-web-tests/src/test/java/ru/relex/education/addressbook/tests/GroupDeleteTest.java
+++ addressbook-web-tests/src/test/java/ru/relex/education/addressbook/tests/GroupDeleteTest.java
@@ -20,5 +20,10 @@ public class GroupDeleteTest extends TestBase{
     app.getGroupsHelperr().returnToGroupPage();
     List<GroupData> after = app.getGroupsHelperr().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    for (int i = 0; i < after.size(); i++) {
+      Assert.assertEquals(before, after);
+    }
   }
 }
