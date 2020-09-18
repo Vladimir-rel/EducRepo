c02567a189d9ce3d7a9bd97976e9e61c8c989467
Kodenko <vladimir_kodenko@relex.ru>
2020-09-18 17:21:12 +0300

    Finish excises for lesson 6.

--- addressbook-web-tests/src/test/java/ru/relex/education/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/relex/education/addressbook/appmanager/ApplicationManager.java
@@ -1,11 +1,10 @@
 package ru.relex.education.addressbook.appmanager;
-
+import com.beust.jcommander.ParameterException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -13,7 +12,6 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-
   private final Properties prorerties;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
@@ -22,7 +20,7 @@ public class ApplicationManager {
   String browser;
   WebDriver wd;
 
-  public ApplicationManager(String browser) throws IOException {
+  public ApplicationManager(String browser) {
     this.browser = browser;
     prorerties = new Properties();
   }
@@ -37,20 +35,22 @@ public class ApplicationManager {
 
   public void init() throws InterruptedException, IOException {
     String target = System.getProperty("target", "local");
-    prorerties.load(new FileReader(new File(String.format("src/test/resources/%s.prorerties", target))));
-    if (browser == BrowserType.CHROME){
+    prorerties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+    if (browser.equals(BrowserType.CHROME)){
       wd = new ChromeDriver();
-    } else if (browser == BrowserType.FIREFOX) {
+    } else if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else if (browser == BrowserType.IE) {
+    } else if (browser.equals(BrowserType.IE)) {
       wd = new InternetExplorerDriver();
+    } else {
+      throw new ParameterException("Unsupported browser");
     }
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(prorerties.getProperty("web.baseUrl"));
     groupHelper = new GroupsHelper(wd);
     contactHelper = new ContactsHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    wd.get(prorerties.getProperty("web.baseUrl"));
     sessionHelper.login(prorerties.getProperty("web.adminLogin"), prorerties.getProperty("web.adminPassword"));
   }
 
--- addressbook-web-tests/src/test/java/ru/relex/education/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/relex/education/addressbook/tests/TestBase.java
@@ -8,17 +8,7 @@ import java.io.IOException;
 
 public class TestBase {
 
-  protected static ApplicationManager app = null;
-
-  static {
-    try {
-      app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-  //protected final static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
-  //protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws InterruptedException, IOException {
