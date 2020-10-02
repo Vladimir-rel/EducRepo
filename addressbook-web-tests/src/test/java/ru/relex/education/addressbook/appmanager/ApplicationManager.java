f99e813bdee3feeced0e5b40cc09c1fd366a7f83
Kodenko <vladimir_kodenko@relex.ru>
2020-10-02 16:07:11 +0300

    Add remote SeleniumServer.

--- addressbook-web-tests/src/test/java/ru/relex/education/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/relex/education/addressbook/appmanager/ApplicationManager.java
@@ -1,13 +1,18 @@
 package ru.relex.education.addressbook.appmanager;
 import com.beust.jcommander.ParameterException;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -44,15 +49,22 @@ public class ApplicationManager {
     prorerties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
     dbHelper = new DbHelper();
-    if (browser.equals(BrowserType.CHROME)){
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
+    if ("".equals(prorerties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.CHROME)){
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      } else {
+        throw new ParameterException("Unsupported browser");
+      }
     } else {
-      throw new ParameterException("Unsupported browser");
+      DesiredCapabilities capabilites = new DesiredCapabilities();
+      capabilites.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(prorerties.getProperty("selenium.server")), capabilites);
     }
+
     groupHelper = new GroupsHelper(wd);
     contactHelper = new ContactsHelper(wd);
     navigationHelper = new NavigationHelper(wd);
